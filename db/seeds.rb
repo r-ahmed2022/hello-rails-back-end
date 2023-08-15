2b9cdafab7ed1f316fe889404ac3640290d55264
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-15 21:18:30 +0530

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-greetings = Greeting.create( [
-    {
-        message: "Hello, welcome"
-    },
-    {
-        message: "Good morning"
-    },
-    {
-        message: "Good to see you back"
-    },
-    {
-        message: "welcome back"
-    },
-    {
-        message: "How're you!"
-    }
-])
+Greeting.create([
+                  {
+                    message: 'Hello, welcome'
+                  },
+                  {
+                    message: 'Good morning'
+                  },
+                  {
+                    message: 'Good to see you back'
+                  },
+                  {
+                    message: 'welcome back'
+                  },
+                  {
+                    message: "How're you!"
+                  }
+                ])
