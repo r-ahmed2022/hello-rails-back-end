cda3aba1f219c5885cc828eb99a75b24d66e1840
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-15 21:26:30 +0530

    updated greetings controller

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class GreetingsController < ApplicationController
-      protect_from_forgery with: :null_session
       def show_greeting
         greeting = Greeting.order('RANDOM()').first
         render json: { message: greeting.message }
